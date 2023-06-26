85f83bfcf3806ef74bb4b7974ec04a237f5ffd4c
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-06-26 16:07:23 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -10,9 +10,9 @@ from ..logging import LOGGER
 
 class AnonXBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"starting iheart music of Aarush...")
         super().__init__(
-            "AnonXMusic",
+            "Aarush Music",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -30,7 +30,7 @@ class AnonXBot(Client):
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "Please promote iheart Bot as Admin in Logger Group"
             )
             sys.exit()
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
