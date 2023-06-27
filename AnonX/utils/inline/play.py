6b5251e39f910c62f66680c5f4ceb5fc1a01bd0d
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-06-27 21:43:19 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -12,7 +12,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(0.5*percentage)
+    anon = math.floor(percentage)
     anonn = math.floor(2*percentage)
     anonnn = math.floor(percentage)
 
