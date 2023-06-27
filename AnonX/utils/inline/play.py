0640603fbd981ffb29f143b9ae43a0572bbd613d
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-06-27 15:04:56 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -12,7 +12,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
+    anon = math.floor(2*percentage)
     if 0 < anon <= 10:
         ba = "⚪─────────"
     elif 10 < anon < 20:
@@ -284,7 +284,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    anon = math.floor(percentage)
+    anon = math.floor(2*percentage)
     if 0 < anon <= 10:
         bar = "⚪─────────"
     elif 10 < anon < 20:
